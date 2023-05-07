745262b6d72a68c6f91b81fd342e5bdf07b42e33
firman <dev.aikosia@gmail.com>
2023-05-07 23:01:21 +0700

    feat: add new options links (-l, --link) and asterisker field array type on package.json

--- README.md
+++ README.md
@@ -1,25 +1,67 @@
-# THE MOTIVATION
-
-1. Change dependencierpackage.json
-
-# GETTING STARTED
-
-## GLOBAL
+#### GLOBAL 
 
 ```
 npm install -g @aikosia/asterisker
 ```
 
-## LOCAL 
+#### LOCAL
 
 ```
 npm install @aikosia/asterisker
 ```
 
-## EXAMPLE
+Assume you have this on your package.json
+
+```
+dependencies:{
+    @aikosia/automaton-core: ^1.0.0
+    @aikosia/automaton-client: ^1.0.0
+}
+```
+
+And then, those two packages have a new release 
+
+1. @aikosia/automaton-core v1.1.0 
+2. @aikosia/automaton-client v1.2.0 
+
+on your root working directory, you can run:
 
 ```
 asterisker @aikosia/automaton-core @aikosia/automaton-cli
 ```
+
+to update those two packages to the new release.
+
+package.json will be updated to:
+
+```
+dependencies:{
+    @aikosia/automaton-core: ^1.1.0
+    @aikosia/automaton-client: ^1.2.0
+}
+```
+
+#### NPX
+
+```
+npx @aikosia/asterisker express mongodb
+```
+
+or you can using like this:
+
+on package.json add asterisker field with array type
+
+```
+{
+    asterisker:["express","mongodb"]
+}
+```
+
+and then run:
+
+```
+npx @aikosia/asterisker -l
+```
+
 
 
--- src/index.js
+++ src/index.js
@@ -10,22 +10,35 @@ program
   .name("asterisker")
   .description("Change dependencies package.json to latest")
   .version("1.0.0")
-  .argument("<name...>","Package name. Example: asterisker @aikosia/automaton-core @aikosia/automaton-cli")
+  .option("-l, --link","Read asterisker field (type array) from package.json",false)
+  .argument("[name...]","Package name. Example: asterisker @aikosia/automaton-core @aikosia/automaton-cli")
   .action(asterisker)
 
 program.parseAsync(process.argv);
 
-async function asterisker(args){
+async function asterisker(args,option){
     try{
-        const dependecies = args.map(val=>val.trim()).filter(val=>val.length);
-        console.log("asterisker",`if ${dependecies} exists then change to '*'`);
+        let dependencies = args.map(val=>val.trim()).filter(val=>val.length);
         const pkg = await readPackage();
+        
+        if(!dependencies.length){
+            if(!(option.asterisker || pkg["asterisker"] || Array.isArray(pkg["asterisker"]))){
+                return;
+            }else{
+                dependencies = pkg["asterisker"].map(val=>val.trim());
+                if(!dependencies.length){
+                    return;
+                }
+            }
+        }
+
+        console.log("asterisker",`if ${dependencies} exists then change to '*'`);
         const specs = ['dependencies','devDependencies','peerDependencies','peerDependenciesMeta','bundleDependencies','optionalDependencies']
         const obj = {};
         specs.forEach((val)=>{
             if(pkg[val] !== undefined){
                 obj[val] = s=>{
-                    dependecies.forEach(element => {
+                    dependencies.forEach(element => {
                         if(s[element] !== undefined){
                             console.log("asterisker",`${element} exists`);
                             s[element] = "*";
