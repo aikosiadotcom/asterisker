b047d22fc3d7538b0cbe61a6782b1c7573f44032
firman <dev.aikosia@gmail.com>
2023-05-07 22:00:21 +0700

    feat(first release): this is first release

---
+++ README.md
@@ -0,0 +1,21 @@
+# THE STORY
+
+# HOW TO USE
+
+## GLOBAL
+
+```
+npm install -g @aikosia/asterisker
+```
+
+```
+asterisker @aikosia/automaton-core, @
+```
+
+## LOCAL 
+
+```
+npm install @aikosia/asterisker
+```
+
+
---
+++ src/index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+import { modifyJsonFile } from "modify-json-file";
+import { execSync }  from 'child_process';
+import path from 'path';
+
+const program = new Command("asterisker");
+program
+  .name("asterisker")
+  .description("Change dependencies package.json to latest")
+  .version("1.0.0")
+  .argument("<name...>","Package name. Example: asterisker @aikosia/automaton-core @aikosia/automaton-cli")
+  .action(asterisker)
+
+program.parseAsync(process.argv);
+
+async function asterisker(args){
+    try{
+        const dependecies = args.map(val=>val.trim()).filter(val=>val.length);
+        console.log("asterisker",`if ${dependecies} exists then change to '*'`);
+        await modifyJsonFile(
+            path.join(process.cwd(), "package.json"),
+            {
+                dependencies: s=>{
+                    dependecies.forEach(element => {
+                        if(s[element] !== undefined){
+                            console.log("asterisker",`${element} exists`);
+                            s[element] = "*";
+                        }
+                    });
+
+                    return s;
+                }
+            }
+        );
+        
+        console.log("asterisker","npm update --save");
+        execSync("npm update --save");
+        console.log("asterisker","done !");
+    }catch(err){
+        console.error("asterisker",err);
+    }
+}
